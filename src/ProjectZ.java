b643eb2394ad16a562dfa50d62b1966b9807ee43
KrisionAyers <ayersk@student.swosu.edu>
2015-10-22 16:25:34 -0500

    Added if statments

--- src/ProjectZ.java
+++ src/ProjectZ.java
@@ -1,9 +1,24 @@
-
+import java.util.Scanner;
 public class ProjectZ {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Enter your age: ");
+		
+		double age = input.nextDouble();
+		System.out.print("Your age is: " + age);
+		
+		boolean Peggy = true;
+		boolean Steven = false;
+		
+		if (Peggy){
+			System.out.print("Hey Peggy.");
+		} else if (Steven){
+			System.out.print("Hey Steven");
+		} else {
+			System.out.print("Who are you?");
+		}
 	}
 
 }
